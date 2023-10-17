bd7096c41243e7b99ed4ffb4431db4f45ff361f9
Phe-nix <88503925+Phe-nix@users.noreply.github.com>
2023-10-18 02:33:10 +0700

    :fire: : Bye

--- src/screens/Friend.jsx
+++
@@ -1,9 +0,0 @@
-import { TextInput, View, Text } from "react-native";
-
-export default function Friend({}) {
-  return (
-    <View className="my-2">
-      <Text>Friend</Text>
-    </View>
-  );
-}
