6843cf05e3eb1d80ba3ab762a1bfdfa58bbfb963
Phe-nix <88503925+Phe-nix@users.noreply.github.com>
2023-10-18 02:33:32 +0700

    :lipstick: : update ui friends

---
+++ src/screens/Friends.jsx
@@ -0,0 +1,82 @@
+import { TextInput, View, Text, ScrollView } from "react-native";
+import SearchBar from "../components/SearchBar";
+import Member from "../components/Member";
+
+const Data = [
+  {
+    id: 1,
+    name: "User1",
+    img: "https://minio.haxter.ee/ctx-betterexperience-prd/uploads/images/221ddf5a-642b-4ace-b145-f9426ab2ad03_original.jpg",
+  },
+  {
+    id: 2,
+    name: "User2",
+    img: "https://play-lh.googleusercontent.com/cShys-AmJ93dB0SV8kE6Fl5eSaf4-qMMZdwEDKI5VEmKAXfzOqbiaeAsqqrEBCTdIEs",
+  },
+  {
+    id: 3,
+    name: "User3",
+    img: "https://play-lh.googleusercontent.com/cShys-AmJ93dB0SV8kE6Fl5eSaf4-qMMZdwEDKI5VEmKAXfzOqbiaeAsqqrEBCTdIEs",
+  },
+  {
+    id: 4,
+    name: "User4",
+    img: "https://play-lh.googleusercontent.com/cShys-AmJ93dB0SV8kE6Fl5eSaf4-qMMZdwEDKI5VEmKAXfzOqbiaeAsqqrEBCTdIEs",
+  },
+  {
+    id: 1,
+    name: "User1",
+    img: "https://minio.haxter.ee/ctx-betterexperience-prd/uploads/images/221ddf5a-642b-4ace-b145-f9426ab2ad03_original.jpg",
+  },
+  {
+    id: 2,
+    name: "User2",
+    img: "https://play-lh.googleusercontent.com/cShys-AmJ93dB0SV8kE6Fl5eSaf4-qMMZdwEDKI5VEmKAXfzOqbiaeAsqqrEBCTdIEs",
+  },
+  {
+    id: 3,
+    name: "User3",
+    img: "https://play-lh.googleusercontent.com/cShys-AmJ93dB0SV8kE6Fl5eSaf4-qMMZdwEDKI5VEmKAXfzOqbiaeAsqqrEBCTdIEs",
+  },
+  {
+    id: 4,
+    name: "User4",
+    img: "https://play-lh.googleusercontent.com/cShys-AmJ93dB0SV8kE6Fl5eSaf4-qMMZdwEDKI5VEmKAXfzOqbiaeAsqqrEBCTdIEs",
+  },
+  {
+    id: 1,
+    name: "User1",
+    img: "https://minio.haxter.ee/ctx-betterexperience-prd/uploads/images/221ddf5a-642b-4ace-b145-f9426ab2ad03_original.jpg",
+  },
+  {
+    id: 2,
+    name: "User2",
+    img: "https://play-lh.googleusercontent.com/cShys-AmJ93dB0SV8kE6Fl5eSaf4-qMMZdwEDKI5VEmKAXfzOqbiaeAsqqrEBCTdIEs",
+  },
+  {
+    id: 3,
+    name: "User3",
+    img: "https://play-lh.googleusercontent.com/cShys-AmJ93dB0SV8kE6Fl5eSaf4-qMMZdwEDKI5VEmKAXfzOqbiaeAsqqrEBCTdIEs",
+  },
+  {
+    id: 4,
+    name: "User4",
+    img: "https://play-lh.googleusercontent.com/cShys-AmJ93dB0SV8kE6Fl5eSaf4-qMMZdwEDKI5VEmKAXfzOqbiaeAsqqrEBCTdIEs",
+  },
+];
+
+export default function Friends({ }) {
+  return (
+    <ScrollView className="overflow-auto w-full h-full px-4 py-4">
+      <View>
+        <SearchBar />
+      </View >
+      <View>
+        <Member 
+        data={Data}
+        memberType={'delete'}
+        />
+      </View>
+    </ScrollView>
+  );
+}
