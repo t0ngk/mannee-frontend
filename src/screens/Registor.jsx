040ab14491b42c10b2f88f36df3530f4698abfb9
Xlaperr <88415760+Xlaperr@users.noreply.github.com>
2023-10-26 15:36:06 +0700

    regis logo

--- src/screens/Registor.jsx
+++ src/screens/Registor.jsx
@@ -1,7 +1,7 @@
 import { View, Image, Button, Alert, TextInput, Text } from "react-native";
 import Input from "../components/Input";
 import { Formik } from "formik";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 export default function Registor({ navigation }) {
   const signup = async (value) => {
@@ -10,113 +10,158 @@ export default function Registor({ navigation }) {
       password: value.password,
       email: value.email,
       confirmPassword: value.confirmPassword,
-    }
-    console.log(data)
-    const api = await fetch('http://localhost:3000/auth/signup', {
-      method: 'POST',
+    };
+    console.log(data);
+    const api = await fetch("http://localhost:3000/auth/signup", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
-    console.log(api)
+    console.log(api);
     if (api.ok) {
       const res = await api.json();
       Alert.alert("Success : " + res.message);
-      navigation.navigate("Login")
-      console.log(res)
+      navigation.navigate("Login");
+      console.log(res);
     } else {
       const err = await api.json();
       Alert.alert("Error : " + err.error);
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   const RegistorSchema = Yup.object().shape({
-    username: Yup.string().min(4, 'Too Short!').max(100, 'Too Long!').required('Required'),
-    password: Yup.string().min(6, 'Too Short!').max(100, 'Too Long!').required('Required'),
-    confirmPassword: Yup.string().equals([Yup.ref('password')], 'Password not match').required('Required'),
-    email: Yup.string().email('Invalid email').required('Required'),
+    username: Yup.string()
+      .min(4, "Too Short!")
+      .max(100, "Too Long!")
+      .required("Required"),
+    password: Yup.string()
+      .min(6, "Too Short!")
+      .max(100, "Too Long!")
+      .required("Required"),
+    confirmPassword: Yup.string()
+      .equals([Yup.ref("password")], "Password not match")
+      .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
   });
 
-
-
   return (
-    <View className="flex flex-col items-center justify-center content-center my-auto py-52" >
+    <View className="flex flex-col items-center justify-center content-center my-auto py-52">
       <View className="mb-5">
-        <Image className="h-20 w-20" source={{ uri: 'https://cdn.logo.com/hotlink-ok/logo-social.png' }} />
+        <Image
+          className="h-40 w-40"
+          source={{ uri: "https://i.imgur.com/sijQJ3j.png" }}
+        />
       </View>
 
       <Formik
         validationSchema={RegistorSchema}
-        initialValues={{ username: '', email: '', password: '', confirmPassword: '' }}
-        onSubmit={async (values) => { await signup(values) }}>
-        {({ handleChange, handleSubmit, values, handleBlur, errors, touched }) => (
+        initialValues={{
+          username: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        onSubmit={async (values) => {
+          await signup(values);
+        }}
+      >
+        {({
+          handleChange,
+          handleSubmit,
+          values,
+          handleBlur,
+          errors,
+          touched,
+        }) => (
           <View className="flex flex-col w-80">
             <View className="py-3">
               <Input
-                onBlur={handleBlur('username')}
+                onBlur={handleBlur("username")}
                 error={errors.username}
                 touched={touched.username}
                 icon="user"
                 placeholder="Your Username"
-                onChangeText={handleChange('username')}
-                value={values.username} />
+                onChangeText={handleChange("username")}
+                value={values.username}
+              />
               {errors.username && touched.username ? (
                 <Text className="text-red-500 pt-1">{errors.username}</Text>
               ) : null}
             </View>
             <View className="py-3">
               <Input
-                onBlur={handleBlur('email')}
+                onBlur={handleBlur("email")}
                 error={errors.email}
                 touched={touched.email}
                 icon="mail"
                 placeholder="Your Email"
-                onChangeText={handleChange('email')}
+                onChangeText={handleChange("email")}
                 value={values.email}
-                keyboardType="email-address" />
+                keyboardType="email-address"
+              />
               {errors.email && touched.email ? (
                 <Text className="text-red-500 pt-1">{errors.email}</Text>
               ) : null}
             </View>
             <View className="py-3">
               <Input
-                onBlur={handleBlur('password')}
+                onBlur={handleBlur("password")}
                 error={errors.password}
                 touched={touched.password}
                 icon="lock"
                 placeholder="Your Password"
-                onChangeText={handleChange('password')}
+                onChangeText={handleChange("password")}
                 value={values.password}
-                secureTextEntry={true} />
+                secureTextEntry={true}
+              />
               {errors.password && touched.password ? (
                 <Text className="text-red-500 pt-1">{errors.password}</Text>
               ) : null}
             </View>
             <View className="py-3">
               <Input
-                onBlur={handleBlur('confirmPassword')}
+                onBlur={handleBlur("confirmPassword")}
                 error={errors.confirmPassword}
                 touched={touched.confirmPassword}
                 icon="lock"
                 secureTextEntry={true}
                 placeholder="Confirm Password"
-                onChangeText={handleChange('confirmPassword')}
-                value={values.confirmPassword} />
+                onChangeText={handleChange("confirmPassword")}
+                value={values.confirmPassword}
+              />
               {errors.confirmPassword && touched.confirmPassword ? (
-                <Text className="text-red-500 pt-1">{errors.confirmPassword}</Text>
+                <Text className="text-red-500 pt-1">
+                  {errors.confirmPassword}
+                </Text>
               ) : null}
             </View>
             <View className="border-2 w-80 rounded-md bg-black mt-3">
-              <Button color="#FFFF" title="Register" onPress={handleSubmit} disabled={errors.username || errors.password || errors.email || errors.confirmPassword}/>
+              <Button
+                color="#FFFF"
+                title="Register"
+                onPress={handleSubmit}
+                disabled={
+                  errors.username ||
+                  errors.password ||
+                  errors.email ||
+                  errors.confirmPassword
+                }
+              />
             </View>
           </View>
-
         )}
       </Formik>
       <View className="border-[1px] w-80 rounded-md mt-3 top-20">
-        <Button color="#000" title="Login" onPress={() => { navigation.navigate("Login") }} />
+        <Button
+          color="#000"
+          title="Login"
+          onPress={() => {
+            navigation.navigate("Login");
+          }}
+        />
       </View>
     </View>
   );
